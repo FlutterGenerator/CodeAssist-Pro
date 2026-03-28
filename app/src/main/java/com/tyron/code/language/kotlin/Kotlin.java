7a8c7d43bca7fe59f6216266bfaf6a59c23a87c2
Wadamzmail <alksndrmetwakil@gmail.com>
2026-03-28 09:53:54 +0200

    add Arabic translate
    Signed-off-by: Wadamzmail <alksndrmetwakil@gmail.com>

--- app/src/main/java/com/tyron/code/language/kotlin/Kotlin.java
+++ app/src/main/java/com/tyron/code/language/kotlin/Kotlin.java
@@ -1,6 +1,9 @@
 package com.tyron.code.language.kotlin;
 
+import com.tyron.code.ApplicationLoader;
 import com.tyron.code.language.Language;
+import com.tyron.common.ApplicationProvider;
+import com.tyron.common.SharedPreferenceKeys;
 import com.tyron.editor.Editor;
 import java.io.File;
 
@@ -12,6 +15,9 @@ public class Kotlin implements Language {
 
   @Override
   public io.github.rosemoe.sora.lang.Language get(Editor editor) {
-    return new KotlinLanguage2(editor);
+    return useFastKotlinLsp() ? new KotlinLanguage2(editor) : new KotlinLanguage(editor);
+  }
+  public boolean useFastKotlinLsp(){
+    return ApplicationLoader.getDefaultPreferences().getBoolean(SharedPreferenceKeys.FAST_KOTLIN_LSP, false);
   }
 }
