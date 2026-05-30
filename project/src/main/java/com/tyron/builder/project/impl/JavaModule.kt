ad7827231937fd9c07e2183d142cb58b195a50c4
MutwakilX <144057106+Wadamzmail@users.noreply.github.com>
2026-05-30 21:48:22 +0200

    fix some files ignored by git & prepare JavaModule
    Signed-off-by: MutwakilX <144057106+Wadamzmail@users.noreply.github.com>

--- project/src/main/java/com/tyron/builder/project/impl/JavaModule.kt
+++ project/src/main/java/com/tyron/builder/project/impl/JavaModule.kt
@@ -87,3 +87,76 @@ fun JavaModuleImpl.searchSourceFileRelatively(file: Path?): SourceClassTrie.Sour
 
     return null
 }
+
+fun JavaModuleImpl.getIntermediateClasspaths(): Set<File> {
+    val result = mutableSetOf<File>()
+//    val variant = getSelectedVariant()?.name ?: "debug"
+
+    val kotlinClasses = File(buildDirectory, "bin/kotlin")
+    if (kotlinClasses.exists()) {
+        result.add(kotlinClasses)
+    }
+
+    val javaClassesDir = File(buildDirectory, "bin/java")
+    if (javaClassesDir.exists()) {
+        javaClassesDir.walkTopDown()
+            .filter { it.name == "classes" && it.isDirectory }
+            .forEach { result.add(it) }
+    }
+
+//    val rClassDir = File(
+//        buildDirectory,
+//        "intermediates/compile_and_runtime_not_namespaced_r_class_jar/$variant"
+//    )
+//    if (rClassDir.exists()) {
+//        rClassDir.walkTopDown()
+//            .filter { it.name == "R.jar" && it.isFile }
+//            .forEach { result.add(it) }
+//    }
+
+    return result
+}
+
+fun JavaModuleImpl.getRuntimeDexFiles(): Set<File> {
+    val result = mutableSetOf<File>()
+//    val variant = getSelectedVariant()?.name ?: "debug"
+
+//    log.info(
+//        "getRuntimeDexFiles: buildDir={}, variant={}",
+//        buildDirectory.absolutePath,
+//        variant
+//    )
+
+    val dexDir = File(buildDirectory, "libraries")
+    Log.i("",String.format("  Checking dexDir: {} (exists: {})", dexDir.absolutePath, dexDir.exists()))
+    if (dexDir.exists()) {
+        dexDir.walkTopDown()
+            .filter { it.name.endsWith(".dex") && it.isFile }
+            .forEach {
+                Log.i("",String.format("    Found DEX: {}", it.absolutePath))
+                result.add(it)
+            }
+    }
+    val lonDex = File(buildDirectory,"bin/classes.dex")
+    if (lonDex.exists()){
+        result.add(lonDex)
+    }
+
+    val mergeProjectDexDir = File(buildDirectory, "intermediates/classes")
+    Log.i("",String.format(
+        "  Checking project_dex_archive: {} (exists: {})",
+        mergeProjectDexDir.absolutePath,
+        mergeProjectDexDir.exists()
+    ))
+    if (mergeProjectDexDir.exists()) {
+        mergeProjectDexDir.walkTopDown()
+            .filter { it.name.endsWith(".dex") && it.isFile }
+            .forEach {
+                Log.i("",String.format("    Found DEX: {}",it.absolutePath))
+                result.add(it)
+            }
+    }
+
+    Log.i("",String.format("  Total DEX files found: {}", result.size))
+    return result
+}
